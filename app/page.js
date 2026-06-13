706785de788d039eab8826dbf57654b79cf8a6e7
Ayaan Baig <baigmohdayaan@gmail.com>
2026-06-13 11:13:50 +0530

    Working with fonts

--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Roboto,Work_Sans} from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  variable:"--font-roboto"
+});
 
 const page = () => {
   return (
-    <div>
-      <h1>Home page</h1>
+    <div className={roboto.className }>
+      <div className="bg-black h-screen w-screen ">
+        <h1 className="text-9xl text-white">Home page</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
